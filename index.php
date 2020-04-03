2d6cc079618a747dcd610fdabfcbfedf8390c948
Caue  Santos <caue.santos@kinghost.com.br>
2020-04-02 21:19:16 -0300

    fist commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.14.1/css/mdb.min.css" rel="stylesheet">
+</head>
+<html>
+
+<body>
+    <div class="container">
+
+        <section class="mt-5 wow fadeIn" style="visibility: visible; animation-name fadeIn;">
+
+            <div class="row">
+
+                <div class="col-lg-4 col-md-12 mx-auto my-5">
+
+                    <!--Card-->
+                    <div class="card card-cascade">
+
+                        <!--Card image-->
+                        <div class="view view-cascade">
+                            <!-- <img src="https://mdbootstrap.com/img/Photos/Others/men.jpg" class="card-img-top" alt=""> -->
+                            <img src="img/IMG_0979.jpg" class="card-img-top" alt="">
+                            <a>
+                                <!-- <div class="mask rgba-white-slight"></div> -->
+                            </a>
+                        </div>
+                        <!--/.Card image-->
+
+                        <!--Card content-->
+                        <div class="card-body card-body-cascade text-center">
+                            <!--Title-->
+                            <h4 class="card-title"><strong>Caue Santos</strong></h4>
+                            <!-- <h5>Web developer</h5> -->
+
+                            <p class="card-text">Desenvolvedor PHP apaixonado Apple e Linux, skatista nas horas vagas e fã (muito) de Monster!
+                            </p>
+
+                            <!--Facebook-->
+                            <a type="button" class="btn-floating btn-small btn-fb"><i class="fab fa-facebook-f"></i></a>
+                            <!--Twitter-->
+                            <a type="button" class="btn-floating btn-small btn-tw"><i class="fab fa-twitter"></i></a>
+                            <!--Google +-->
+                            <a type="button" class="btn-floating btn-small btn-dribbble"><i class="fab fa-dribbble"></i></a>
+
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
+
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
